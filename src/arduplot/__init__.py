0c2ee4368ba957bb2828a9b6a8f32502e100039c
Yoonseok Hur <yoonseok@gmail.com>
2021-10-02 12:51:34 +0900

    arduplot with click

--- src/arduplot/__init__.py
+++ src/arduplot/__init__.py
@@ -18,4 +18,4 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-from serialplotter.serialplotter import main
+from arduplot.plotserialdata import main
