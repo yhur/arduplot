3febcddd7ee4ddefc45a28c801793c2b8a4efe3c
Yoonseok Hur <yoonseok@gmail.com>
2021-10-21 14:44:03 +0900

    initial lint check

--- src/arduplot/plotserialdata.py
+++ src/arduplot/plotserialdata.py
@@ -1,50 +1,52 @@
 #!/usr/bin/env python3
-# Copyright (c) <year> <copyright holders>
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+'''
+ Copyright (c) <year> <copyright holders>
 
-import serial
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+'''
+
 import os
+import sys
 import signal
 import json
 import socket
+import matplotlib.pyplot as plt
+from matplotlib import animation
+import serial
 import click
 from serial.serialutil import SerialException
 try:
     from platformio.project.config import ProjectConfig
-    piomode = True
+    PIO_MODE = True
 except ImportError:
-    piomode = False
+    PIO_MODE = False
 
-# Handle Ctrl-C
 def sighandler(signum, frame):
-    exit(9)
+    '''signal handler for Ctrl-C'''
+    sys.exit(9)
 signal.signal(signal.SIGINT, sighandler)
 
-# utility function to get the plotter configuration setting
-def valueByKey(j, key, value):
+def value_by_key(j, key, value):
+    '''utility function to get the plotter configuration setting'''
     if key in j:
         return j[key]
-    else:
-        return value
+    return value
 
 # BEGIN MAIN FUNCTION
 @click.command()
@@ -55,6 +57,7 @@ def valueByKey(j, key, value):
 @click.option("--baud", "-b", type=int, help="Set baudrate, default=115200")
 @click.argument("labels", nargs=-1)
 def main(**kwargs):
+    '''main function'''
     # Reading data function from the Serial Port
     def uart_in():
         return ser.readline()
@@ -66,30 +69,30 @@ def main(**kwargs):
     # Callback function for plotting the data by animation.FuncAnimation
     def animate(self):
         ax.clear()
-        line = getInput().split()
+        lines = get_input().split()
         # data labelling
-        if len(line) > len(data_label):             
-            i = len(line) - len(data_label)
+        if len(lines) > len(data_label):
+            i = len(lines) - len(data_label)
             j = 0
             while i > j:
                 data_label.append('data' + str(j + 1))
                 j = j + 1
-    
+
         # data array preparation
         k = 0
-        for d in line:
+        for l in lines:
             try:
-                d = float(d)
+                l = float(l)
             except ValueError:
-                print("Can't convert {} to float. it's zeroed out".format(d))
-                d = float(0)
+                print(f"Can't convert {l} to float. it's zeroed out")
+                l = float(0)
             if len(data) <= k:
                 data.append([])
-            data[k].append(d)
+            data[k].append(l)
             data[k] = data[k][-width:]              # truncate to the graph width
             ax.plot(data[k], label=data_label[k])
             k = k + 1
-    
+
         # plotting
         plt.title(title)
         plt.xticks(rotation=90, ha='right')
@@ -108,31 +111,31 @@ def main(**kwargs):
 
     # check and get the plotter config if the config file exists
     try:
-        with open('plotcfg.json') as jfile:
+        with open('plotcfg.json', 'r', encoding='utf-8') as jfile:
             plot_cfg = json.load(jfile)
-        title = valueByKey(plot_cfg, 'title', title)
-        width = valueByKey(plot_cfg, 'width', width)
-        data_label = valueByKey(plot_cfg, 'label', data_label)
+        title = value_by_key(plot_cfg, 'title', title)
+        width = value_by_key(plot_cfg, 'width', width)
+        data_label = value_by_key(plot_cfg, 'label', data_label)
     except FileNotFoundError:
         pass
     title = kwargs['title'] or title
     width = kwargs['width'] or width
     data_label = list(kwargs['labels']) or data_label
-    
+
     if tcp_socket:
-        getInput = tcp_in
+        get_input = tcp_in
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server_socket.bind(('localhost', tcp_socket))
         server_socket.listen()
         client_socket, addr = server_socket.accept()
     else:
-        getInput = uart_in
+        get_input = uart_in
         ser = serial.Serial()
         ser.timeout = 10
         ser.port = None
         ser.baudrate = 115200
-        if piomode:
+        if PIO_MODE:
             if os.path.isfile(ProjectConfig.get_default_path()):
                 config = ProjectConfig.get_instance()  # PIO project config
                 for s in config.sections():
@@ -140,24 +143,24 @@ def main(**kwargs):
                     ser.baudrate = config.get(s, 'monitor_speed') or ser.baudrate
             ser.port = kwargs['port'] or ser.port
             ser.baudrate = kwargs['baud'] or ser.baudrate
-            if ser.port == None:
+            if ser.port is None:
                 print("Please check the platformio.ini for the 'monitor_port or the -p option")
-                exit(2)
+                sys.exit(2)
         else:
             ser.baudrate = kwargs['baud'] or ser.baudrate
             ser.port = kwargs['port']
         if not ser.port:
             print('\nPlease provide the serial port information\n')
             print('\t arduplot -p /dev/cu.usbserail-ABCDEEF or arduplot -p COM3\n')
-            exit(3)
+            sys.exit(3)
         try:
             ser.open()
-            if ser.is_open==True:
-        	    print('\nSerial port listening:')
-        	    print('\tport: {}, baud: {}\n'.format(ser.port, ser.baudrate))
-        except serial.serialutil.SerialException:
-            print('Serial Device {} is not found'.format(ser.port))
-            exit(4)
+            if ser.is_open is True:
+                print('\nSerial port listening:')
+                print(f'\tport: {ser.port}, baud: {ser.baudrate}\n')
+        except SerialException:
+            print(f'Serial Device {ser.port} is not found')
+            sys.exit(4)
 
     fig = plt.figure()
     if not tcp_socket:
